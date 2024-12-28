5a74e3bd0b5419ccf7bb0e17bc7631b84962a925
Ro706 <rohitmandal99091@gmail.com>
2024-12-29 02:28:07 +0530

    adding  jwt token system

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
-
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "Ro706isagoodb$oy";
 // Create a User using : POST "/api/auth/". Doesn't require Auth
 router.post(
     '/',
@@ -25,14 +27,25 @@ router.post(
                 return res.status(400).json({ error: "User with this email already exists" });
             }
 
+            // Hash the password
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
             // Create a new user
             const user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password,
+                password: secPass,
             });
-
-            res.json(user);
+            
+            // Create a token
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authtoken = jwt.sign(data, JWT_SECRET);
+            res.json({authtoken});
         } catch (error) {
             console.error(error.message);
             res.status(500).send("Internal Server Error");
