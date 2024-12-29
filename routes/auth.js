1a374accf482082836e00276162e0c8be132c460
Ro706 <rohitmandal99091@gmail.com>
2024-12-29 20:46:08 +0530

    adding login and getuser in auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const fetchuser = require('../middleware/fetchuser');
 const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = "Ro706isagoodb$oy";
-// Create a User using : POST "/api/auth/". Doesn't require Auth
+// route 1: Create a User using : POST "/api/auth/". Doesn't require Auth
 router.post(
     '/',
     [
@@ -37,7 +38,7 @@ router.post(
                 email: req.body.email,
                 password: secPass,
             });
-            
+
             // Create a token
             const data = {
                 user: {
@@ -45,12 +46,59 @@ router.post(
                 }
             }
             const authtoken = jwt.sign(data, JWT_SECRET);
-            res.json({authtoken});
+            res.json({ authtoken });
         } catch (error) {
             console.error(error.message);
             res.status(500).send("Internal Server Error");
         }
     }
 );
+// route 2: Authenticate a User using : POST "/api/auth/login". No login required
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+    // Validate request body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
+    try {
+        // Check if the user already exists in the database
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({ error: "Invalid Credentials" });
+        }
+
+        // Compare the password
+        const passwordCompare = await bcrypt.compare(req.body.password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Invalid Credentials" });
+        }
+
+        // Create a token
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({ authtoken });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+// route 3: Get logged in user details using : POST "/api/auth/getuser". Login required
+router.post('/getuser',fetchuser ,async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+        res.send(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
 module.exports = router;
